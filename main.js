968a446a26ba4a61a47647a5523e2f4980d67637
Bismark Ade <bismarkade@gmail.com>
2021-12-03 04:48:50 +0100

    Updated with sidebar

--- main.js
+++ main.js
@@ -3,11 +3,6 @@ window.onload = init
 function init(){
 
 
-   var Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
-	maxZoom: 16
-});
-
 var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 19,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
